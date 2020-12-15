b8d48ce2ab9d2a9ab2f3d5bdf13cd3f196e6f171
skearns19 <48564861+skearns19@users.noreply.github.com>
2020-12-14 22:40:22 -0500

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,248 +1,324 @@
-let db;
-let studentdirectory;
+let db = null;
+let studentdirectory = null;
 db.studentdirectory.insert([{
-  "personalInformation": {
-    "name": [{
-      "first": "Jacob",
-      "middle": "Ryan",
-      "last": "Davidson",
-      "preferred": "Jacob"
-    }],
-    "phone": "2036487951",
-    "emailAddress": "Davidson@hartford.edu"
-  },
-  "schoolInformation": {
-    "studentID": 19346872,
-    "curriculum": [{
-      "major": "Computer Science",
-      "minor": "None"}],
-    "advisor": "Professor Rosiene"
-  },
-  "graduationStatus": {
-    "year": "fourth-year",
-    "gpa": 3.66,
-    "studentStatus": "full-time",
-    "dateOfInitialEnrollment": "September 2013",
-    "graduationDate": "May 2017"}
+  "personalInformation":
+{
+  "name":
+{
+  "first": "Jacob",
+  "middle": "Ryan",
+  "last": "Davidson",
+  "preferred": "Jacob"
 },
-  {
-    "personalInformation": {
-      "name": [{
-        "first": "Andrew",
-        "middle": "Michael",
-        "last": "Valentine",
-        "preferred": "Andy"
-      }],
-      "phone": "8607356644",
-      "emailAddress": "Valentine@hartford.edu"
-    },
-    "schoolInformation": {
-      "studentID":19672234,
-      "curriculum": [{
-        "major": "Mathematics",
-        "minor": "English"}],
-      "advisor": "Professor Donald Knuth"
-    },
-    "graduationStatus": {
-      "year": "third-year",
-      "gpa": 3.85,
-      "studentStatus": "part-time",
-      "dateOfInitialEnrollment": "September 2016",
-      "graduationDate": "May 2021"}
+  "phone": "2036487951",
+  "emailAddress": "davidson@hartford.edu"
+},
+  "schoolInformation":
+{
+  "studentID": 19346872,
+  "curriculum":
+{
+  "major": "Computer Science",
+  "minor": "None"
+},
+  "advisor": "Professor Rosiene"
+},
+  "graduationStatus":
+{
+  "year": "fourth-year",
+  "gpa": 3.66,
+  "studentStatus": "full-time",
+  "dateOfInitialEnrollment": "September 2013",
+  "graduationDate": "May 2017"
+}
+},
+{
+  "personalInformation":
+{
+  "name":
+{
+  "first": "Andrew",
+  "middle": "Michael",
+  "last": "Valentine",
+  "preferred": "Andy"
+},
+  "phone": "8607356644",
+  "emailAddress": "valentine@hartford.edu"
+},
+  "schoolInformation":
+{
+  "studentID":19672234,
+  "curriculum":
+{
+  "major": "Mathematics",
+  "minor": "English"
+},
+  "advisor": "Professor Donald Knuth"
+},
+  "graduationStatus":
+{
+  "year": "third-year",
+  "gpa": 3.85,
+  "studentStatus": "part-time",
+  "dateOfInitialEnrollment": "September 2016",
+  "graduationDate": "May 2021"
+}
+},
+{
+  "personalInformation":
+{
+  "name":
+{
+  "first": "Thomas",
+  "middle": "Joseph",
+  "last": "Bradley",
+  "preferred":"Tom"
+},
+  "phone":"2037645467",
+  "emailAddress":"tbradley@hartford.edu"
+},
+  "schoolInformation":
+{
+  "studentID": 19467965,
+  "curriculum": {
+  "major": "Vocal Performance",
+  "minor": "None"},
+  "advisor": "Professor Andrews"
+},
+  "graduationStatus":
+{
+  "year": "first-year",
+  "gpa": 4.00,
+  "studentStatus": "full-time",
+  "dateOfInitialEnrollment": "Sept 2014",
+  "graduationDate": "May 2018"
+}
+},
+{
+  "personalInformation":
+{
+  "name":
+{
+  "first": "John",
+  "middle": "Victor",
+  "last": "Robbins",
+  "preferred":"Jack"
+},
+  "phone":"2038571093",
+  "emailAddress":"Jorobbins@hartford.edu"
+},
+  "schoolInformation":
+{
+  "studentID": 19678843,
+  "curriculum":
+{
+  "major": "Mathematics",
+  "minor": "Computer Science"
+},
+  "advisor": "Professor Haruta"
+},
+  "graduationStatus":
+{
+  "year": "fourth-year",
+  "gpa": 3.50,
+  "studentStatus": "full-time",
+  "dateOfInitialEnrollment": "Sept 2016",
+  "graduationDate": "May 2020"
+}
+},
+{
+  "personalInformation":
+{
+  "name":
+{
+  "first": "Valerie",
+  "middle": "Sarah",
+  "last": "Martinez",
+  "preferred": "Val"
+},
+  "phone": "4132298763",
+  "emailAddress": "martinez@hartford.edu"
+},
+  "schoolInformation":
+{
+  "studentID": 19769875,
+  "curriculum":
+{
+  "major": "Mathematics",
+  "minor": "Physics"
+},
+  "advisor": "Professor Turpin"
+},
+  "graduationStatus":
+{
+  "year": "second-year",
+  "gpa": 3.60,
+  "studentStatus": "part-time",
+  "dateOfInitialEnrollment": "Sept 2017",
+  "graduationDate": "May 2022"
+}
+},
+{
+  "personalInformation":
+{
+  "name":
+{
+  "first": "Danielle",
+  "middle": "Christina",
+  "last": "Jackson",
+  "preferred": "Dani"
+},
+  "phone": "2037428641",
+  "emailAddress": "jackson@hartford.edu"
+},
+  "schoolInformation":
+{
+  "studentID": 19863345,
+  "curriculum":
+{
+  "major": "Computer Engineering",
+  "minor": "None"
+},
+  "advisor": "Professor Yang"
+},
+  "graduationStatus":
+{
+  "year": "Second-year",
+  "gpa": 3.75,
+  "studentStatus": "full-time",
+  "dateOfInitialEnrollment": "Sept 2018",
+  "graduationDate": "May 2022"
+}
+},
+{
+  "personalInformation":
+{
+  "name":
+{
+  "first": "Lily",
+  "middle": "Evelyn",
+  "last": "Roberts",
+  "preferred": "Lily"
+},
+  "phone": "8605486221",
+  "emailAddress": "lroberts@hartford.edu"
+},
+  "schoolInformation":
+{
+  "studentID": 19950933,
+  "curriculum":
+{
+  "major": "Mathematics",
+  "minor": "None"
+},
+  "advisor": "Professor Haruta"
+},
+  "graduationStatus":
+{
+  "year": "fourth-year",
+  "gpa": 3.25,
+  "studentStatus": "part-time",
+  "dateOfInitialEnrollment": "Sept 2019",
+  "graduationDate": "Dec 2023"
+}
+},
+{
+  "personalInformation":
+{
+  "name":
+{
+  "first": "Michelle",
+  "middle": "Lisa",
+  "last": "Nelson",
+  "preferred": "Mickey"
+},
+  "phone": "4139283304",
+  "emailAddress": "mnelson@hartford.edu"
+},
+  "schoolInformation":
+{
+  "studentID": 19384487,
+  "curriculum":
+{
+  "major": "Computer Science",
+  "minor": "Mathematics"
+},
+  "advisor": "Professor Donald Knuth"
+},
+  "graduationStatus":
+{
+  "year": "Second-year",
+  "gpa": 2.75,
+  "studentStatus": "full-time",
+  "dateOfInitialEnrollment": "Sept 2013",
+  "graduationDate": "May 2017"
+}
+},
+{
+  "personalInformation":
+{
+  "name":
+{
+  "first": "Stephen",
+  "middle": "Michael",
+  "last": "Jones",
+  "preferred": "Mike"
+},
+  "phone": "8603937479",
+  "emailAddress": "mijones@hartford.edu"
+},
+  "schoolInformation":
+{
+  "studentID": 19853974,
+  "curriculum":
+{
+  "major": "Computer Science",
+  "minor": "None"
+},
+  "advisor": "Professor Russell"
+},
+  "graduationStatus":
+{
+  "year": "third-year",
+  "gpa": 1.83,
+  "studentStatus": "part-time",
+  "dateOfInitialEnrollment": "Sept 2018",
+  "graduationDate": "May 2023"
+}
+},
+{
+  "personalInformation":
+{
+  "name":
+{
+  "first": "Victoria",
+  "middle": "Jessica",
+  "last": "Nunez",
+  "preferred": "Vicki"
+},
+  "phone": "4139387743",
+  "emailAddress": "nunez@hartford.edu"
+},
+  "schoolInformation":
+{
+  "studentID": 19874643,
+  "curriculum":
+{
+  "major": "Computer Science",
+  "minor": "Mathematics"
   },
-  {"personalInformation": {
-      "name": [{
-        "first": "Thomas",
-        "middle": "Joseph",
-        "last": "Bradley",
-        "preferred":"Tom"
-      }],
-      "phone":"2037645467",
-      "emailAddress":"Tbradley@hartford.edu"
-    },
-    "schoolInformation": {
-      "studentID": 19467965,
-      "curriculum": [{
-        "major": "Vocal Performance",
-        "minor": "None"}],
-      "advisor": "Professor Andrews"
-    },
-    "graduationStatus": {
-      "year": "first-year",
-      "gpa": 4.00,
-      "studentStatus": "full-time",
-      "dateOfInitialEnrollment": "Sept 2014",
-      "graduationDate": "May 2018"}
-  },
-  {"personalInformation": {
-      "name": [{
-        "first": "John",
-        "middle": "Victor",
-        "last": "Robbins",
-        "preferred":"Jack"
-      }],
-      "phone":"2038571093",
-      "emailAddress":"Jorobbins@hartford.edu"
-    },
-    "schoolInformation": {
-      "studentID": 19678843,
-      "curriculum": [{
-        "major": "Mathematics",
-        "minor": "Computer Science"}],
-      "advisor": "Professor Haruta"
-    },
-    "graduationStatus": {
-      "year": "fourth-year",
-      "gpa": 3.50,
-      "studentStatus": "full-time",
-      "dateOfInitialEnrollment": "Sept 2016",
-      "graduationDate": "May 2020"}
-  },
-  {"personalInformation": {
-      "name": [{
-        "first": "Valerie",
-        "middle": "Sarah",
-        "last": "Martinez",
-        "preferred": "Val"
-      }],
-      "phone": "4132298763",
-      "emailAddress": "Martinez@hartford.edu"
-    },
-    "schoolInformation": {
-      "studentID": 19769875,
-      "curriculum": [{
-        "major": "Mathematics",
-        "minor": "Physics"}],
-      "advisor": "Professor Turpin"
-    },
-    "graduationStatus": {
-      "year": "second-year",
-      "gpa": 3.60,
-      "studentStatus": "part-time",
-      "dateOfInitialEnrollment": "Sept 2017",
-      "graduationDate": "May 2022"}
-  },
-  {"personalInformation": {
-      "name": [{
-        "first": "Danielle",
-        "middle": "Christina",
-        "last": "Jackson",
-        "preferred": "Dani"
-      }],
-      "phone": "2037428641",
-      "emailAddress": "Jackson@hartford.edu"
-    },
-    "schoolInformation": {
-      "studentID": 19863345,
-      "curriculum": [{
-        "major": "Computer Engineering",
-        "minor": "None"}],
-      "advisor": "Professor Yang"
-    },
-    "graduationStatus": {
-      "year": "Second-year",
-      "gpa": 3.75,
-      "studentStatus": "full-time",
-      "dateOfInitialEnrollment": "Sept 2018",
-      "graduationDate": "May 2022"}
-  },
-  {"personalInformation": {
-      "name": [{
-        "first": "Lily",
-        "middle": "Evelyn",
-        "last": "Roberts",
-        "preferred": "Lily"
-      }],
-      "phone": "8605486221",
-      "emailAddress": "Lroberts@hartford.edu"
-    },
-    "schoolInformation": {
-      "studentID": 19950933,
-      "curriculum": [{
-        "major": "Mathematics",
-        "minor": "None"}],
-      "advisor": "Professor Haruta"
-    },
-    "graduationStatus": {
-      "year": "fourth-year",
-      "gpa": 3.25,
-      "studentStatus": "part-time",
-      "dateOfInitialEnrollment": "Sept 2019",
-      "graduationDate": "Dec 2023"}
-  },
-  {"personalInformation": {
-      "name": [{
-        "first": "Michelle",
-        "middle": "Lisa",
-        "last": "Nelson",
-        "preferred": "Mickey"
-      }],
-      "phone": "4139283304",
-      "emailAddress": "MNelson@hartford.edu"
-    },
-    "schoolInformation": {
-      "studentID": 19384487,
-      "curriculum": [{
-        "major": "Computer Science",
-        "minor": "Mathematics"}],
-      "advisor": "Professor Donald Knuth"
-    },
-    "graduationStatus": {
-      "year": "Second-year",
-      "gpa": 2.75,
-      "studentStatus": "full-time",
-      "dateOfInitialEnrollment": "Sept 2013",
-      "graduationDate": "May 2017"}
-  },
-  {"personalInformation": {
-      "name": [{
-        "first": "Stephen",
-        "middle": "Michael",
-        "last": "Jones",
-        "preferred": "Mike"
-      }],
-      "phone": "8603937479",
-      "emailAddress": "MiJones@hartford.edu"
-    },
-    "schoolInformation": {
-      "studentID": 19853974,
-      "curriculum": [{
-        "major": "Computer Science",
-        "minor": "None"}],
-      "advisor": "Professor Russell"
-    },
-    "graduationStatus": {
-      "year": "third-year",
-      "gpa": 1.83,
-      "studentStatus": "part-time",
-      "dateOfInitialEnrollment": "Sept 2018",
-      "graduationDate": "May 2023"}
-  },
-  {"personalInformation": {
-      "name": [{
-        "first": "Victoria",
-        "middle": "Jessica",
-        "last": "Nunez",
-        "preferred": "Vicki"
-      }],
-      "phone": "4139387743",
-      "emailAddress": "Nunez@hartford.edu"
-    },
-    "schoolInformation": {
-      "studentID": 19874643,
-      "curriculum": [{
-        "major": "Computer Science",
-        "minor": "Mathematics"}],
-      "advisor": "Professor Russell"
-    },
-    "graduationStatus": {
-      "year": "third-year",
-      "gpa": 1.00,
-      "studentStatus": "part-time",
-      "dateOfInitialEnrollment": "Sept 2018",
-      "graduationDate": "May 2023"}
-  }
+  "advisor": "Professor Russell"
+},
+  "graduationStatus":
+{
+  "year": "third-year",
+  "gpa": 1.00,
+  "studentStatus": "part-time",
+  "dateOfInitialEnrollment": "Sept 2018",
+  "graduationDate": "May 2023"
+}
+}
 ]);
 db.studentdirectory.find({"graduationStatus.gpa": {"$gt":3.50}}).pretty();
-db.studentdirectory.update({"personalInformation.emailAddress":"Valentine@hartford.edu"}, {"$set":{"graduationStatus.studentStatus":"graduated"}});
-db.studentdirectory.update({"personalInformation.emailAddress":"MNelson@hartford.edu"}, {"$set":{"graduationStatus.studentStatus":"withdrawn"}});
+db.studentdirectory.update({"personalInformation.emailAddress":"valentine@hartford.edu"}, {"$set":{"graduationStatus.studentStatus":"graduated"}});
+db.studentdirectory.update({"personalInformation.emailAddress":"mnelson@hartford.edu"}, {"$set":{"graduationStatus.studentStatus":"withdrawn"}});
